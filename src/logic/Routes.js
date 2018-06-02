389f97d920578911c653eb16540ee3e90d0f2e82
karmamocro <o.elhajoui@student.fontys.nl>
2018-06-03 01:54:20 +0200

    well created partiatlly the routes page

---
+++ src/logic/Routes.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import { Route, Switch, withRouter } from "react-router-dom";
+
+import Layout from "../components/Layout";
+
+import { PrivateRoute } from "./Libary";
+
+import { connect } from "react-redux";
+import { logIn } from "../actions/loginAction";
+import { checkLogin } from "../actions/checkLogin";
+
+class Routes extends Component {
+    render() {
+        return (
+            <Layout>
+                <Switch>
+
+                </Switch>
+            </Layout>
+        );
+    }
+}
+
+export default Routes;
